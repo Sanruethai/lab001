600ec67f9591112e91d0effd313cca8215743ad9
Natthawan31 <Natthawan.p@kkumail.com>
2022-12-26 21:42:44 +0700

    Add project files.

---
+++ lab001/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace lab001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.met = new System.Windows.Forms.TextBox();
+            this.seven = new System.Windows.Forms.Button();
+            this.eigt = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.tree = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.max = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.ans = new System.Windows.Forms.Button();
+            this.retor = new System.Windows.Forms.Label();
+            this.tetDisplay2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // met
+            // 
+            this.met.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.met.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.met.ForeColor = System.Drawing.Color.Black;
+            this.met.Location = new System.Drawing.Point(50, 67);
+            this.met.Name = "met";
+            this.met.Size = new System.Drawing.Size(318, 22);
+            this.met.TabIndex = 0;
+            this.met.Text = "0";
+            this.met.TextChanged += new System.EventHandler(this.met_TextChanged);
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.seven.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.seven.ForeColor = System.Drawing.Color.Black;
+            this.seven.Location = new System.Drawing.Point(50, 99);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(75, 38);
+            this.seven.TabIndex = 2;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.total);
+            // 
+            // eigt
+            // 
+            this.eigt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.eigt.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.eigt.ForeColor = System.Drawing.Color.Black;
+            this.eigt.Location = new System.Drawing.Point(131, 99);
+            this.eigt.Name = "eigt";
+            this.eigt.Size = new System.Drawing.Size(75, 38);
+            this.eigt.TabIndex = 3;
+            this.eigt.Text = "8";
+            this.eigt.UseVisualStyleBackColor = false;
+            this.eigt.Click += new System.EventHandler(this.total);
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.nine.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.nine.ForeColor = System.Drawing.Color.Black;
+            this.nine.Location = new System.Drawing.Point(212, 99);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(75, 38);
+            this.nine.TabIndex = 4;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = false;
+            this.nine.Click += new System.EventHandler(this.total);
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.four.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.four.ForeColor = System.Drawing.Color.Black;
+            this.four.Location = new System.Drawing.Point(50, 146);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(75, 39);
+            this.four.TabIndex = 5;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = false;
+            this.four.Click += new System.EventHandler(this.total);
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.five.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.five.ForeColor = System.Drawing.Color.Black;
+            this.five.Location = new System.Drawing.Point(131, 146);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(75, 37);
+            this.five.TabIndex = 6;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = false;
+            this.five.Click += new System.EventHandler(this.total);
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.six.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.six.ForeColor = System.Drawing.Color.Black;
+            this.six.Location = new System.Drawing.Point(212, 146);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(75, 39);
+            this.six.TabIndex = 7;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = false;
+            this.six.Click += new System.EventHandler(this.total);
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.one.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.one.ForeColor = System.Drawing.Color.Black;
+            this.one.Location = new System.Drawing.Point(50, 191);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(75, 39);
+            this.one.TabIndex = 8;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = false;
+            this.one.Click += new System.EventHandler(this.total);
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.two.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.two.ForeColor = System.Drawing.Color.Black;
+            this.two.Location = new System.Drawing.Point(131, 191);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(75, 39);
+            this.two.TabIndex = 9;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = false;
+            this.two.Click += new System.EventHandler(this.total);
+            // 
+            // tree
+            // 
+            this.tree.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.tree.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tree.ForeColor = System.Drawing.Color.Black;
+            this.tree.Location = new System.Drawing.Point(212, 191);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(75, 39);
+            this.tree.TabIndex = 10;
+            this.tree.Text = "3";
+            this.tree.UseVisualStyleBackColor = false;
+            this.tree.Click += new System.EventHandler(this.total);
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.zero.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.zero.ForeColor = System.Drawing.Color.Black;
+            this.zero.Location = new System.Drawing.Point(50, 236);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(75, 39);
+            this.zero.TabIndex = 11;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = false;
+            this.zero.Click += new System.EventHandler(this.total);
+            // 
+            // div
+            // 
+            this.div.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.div.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.div.ForeColor = System.Drawing.Color.Black;
+            this.div.Location = new System.Drawing.Point(293, 100);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(75, 40);
+            this.div.TabIndex = 12;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = false;
+            this.div.AutoSizeChanged += new System.EventHandler(this.BtnMath);
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // mul
+            // 
+            this.mul.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.mul.ForeColor = System.Drawing.Color.Black;
+            this.mul.Location = new System.Drawing.Point(293, 146);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(75, 37);
+            this.mul.TabIndex = 13;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = false;
+            this.mul.AutoSizeChanged += new System.EventHandler(this.BtnMath);
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // min
+            // 
+            this.min.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.min.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.min.ForeColor = System.Drawing.Color.Black;
+            this.min.Location = new System.Drawing.Point(293, 236);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(75, 39);
+            this.min.TabIndex = 14;
+            this.min.Text = "-";
+            this.min.UseVisualStyleBackColor = false;
+            this.min.AutoSizeChanged += new System.EventHandler(this.BtnMath);
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // max
+            // 
+            this.max.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.max.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.max.ForeColor = System.Drawing.Color.Black;
+            this.max.Location = new System.Drawing.Point(293, 191);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(75, 39);
+            this.max.TabIndex = 15;
+            this.max.Text = "+";
+            this.max.UseVisualStyleBackColor = false;
+            this.max.AutoSizeChanged += new System.EventHandler(this.BtnMath);
+            this.max.Click += new System.EventHandler(this.max_Click);
+            // 
+            // point
+            // 
+            this.point.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.point.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.point.ForeColor = System.Drawing.Color.Black;
+            this.point.Location = new System.Drawing.Point(131, 237);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(75, 38);
+            this.point.TabIndex = 16;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = false;
+            this.point.Click += new System.EventHandler(this.total);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.clear.ForeColor = System.Drawing.Color.Black;
+            this.clear.Location = new System.Drawing.Point(374, 100);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(107, 175);
+            this.clear.TabIndex = 18;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // ans
+            // 
+            this.ans.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ans.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.ans.ForeColor = System.Drawing.Color.Black;
+            this.ans.Location = new System.Drawing.Point(212, 237);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(75, 38);
+            this.ans.TabIndex = 21;
+            this.ans.Text = "=";
+            this.ans.UseVisualStyleBackColor = false;
+            this.ans.Click += new System.EventHandler(this.ans_Click);
+            // 
+            // retor
+            // 
+            this.retor.AutoSize = true;
+            this.retor.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.retor.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.retor.ForeColor = System.Drawing.Color.Black;
+            this.retor.Location = new System.Drawing.Point(57, 48);
+            this.retor.Name = "retor";
+            this.retor.Size = new System.Drawing.Size(50, 16);
+            this.retor.TabIndex = 22;
+            this.retor.Text = "labei1";
+            this.retor.Click += new System.EventHandler(this.retor_Click);
+            // 
+            // tetDisplay2
+            // 
+            this.tetDisplay2.Location = new System.Drawing.Point(50, 42);
+            this.tetDisplay2.Name = "tetDisplay2";
+            this.tetDisplay2.Size = new System.Drawing.Size(100, 22);
+            this.tetDisplay2.TabIndex = 23;
+            this.tetDisplay2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(608, 409);
+            this.Controls.Add(this.tetDisplay2);
+            this.Controls.Add(this.retor);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.tree);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eigt);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.met);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Click += new System.EventHandler(this.total);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox met;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eigt;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button tree;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button max;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button ans;
+        private System.Windows.Forms.Label retor;
+        private System.Windows.Forms.TextBox tetDisplay2;
+    }
+}
+
